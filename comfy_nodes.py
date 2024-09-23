b81ec6971b1191ae30f6aef1ffc9e54cb9d0f3e8
unknown <sarthak.agrawal1311@gmail.com>
2024-09-23 17:42:03 +0530

    modified aspectlatent to also incorporate non-aspect ratio usecase

--- comfy_nodes.py
+++ comfy_nodes.py
@@ -11,6 +11,9 @@ class AspectEmptyLatentImage:
     def INPUT_TYPES(s):
         return {
             "required": { 
+                "width": ("INT", {"default":1024, "min":16, "max":MAX_RESOLUTION, "step":8}),
+                "height": ("INT", {"default":1024, "min":16, "max":MAX_RESOLUTION, "step":8}),
+                "aspect_control" : ("BOOLEAN", {"default":False, "tooltip":"Set to 'True' if you want to select size based on a particular aspect ratio."}),
                 "aspect_width_ratio" : ("INT",{"default":9, "min":0}),
                 "aspect_height_ratio" : ("INT",{"default":16, "min":0}),
                 "width_override": ("INT", {"default": 0, "min": 0, "max": MAX_RESOLUTION, "step": 8}),
@@ -24,20 +27,24 @@ class AspectEmptyLatentImage:
     CATEGORY = "IamME"
     DESCRIPTION = "Create a new batch of empty latent images to be denoised via sampling."
 
-    def aspect_latent_gen(self, aspect_width_ratio, aspect_height_ratio, width_override, batch_size=1):
-        if width_override > 0:
-            width = width_override
-            height = int((aspect_height_ratio * width) / aspect_width_ratio)
-        else:
-            base_size = 1024
-            if aspect_width_ratio <= aspect_height_ratio:
-                width = base_size
-                height = int((aspect_height_ratio * width) / aspect_width_ratio)
+    def aspect_latent_gen(self, width, height, aspect_control, aspect_width_ratio, aspect_height_ratio, width_override, batch_size=1):
+        if aspect_control==True:
+            if width_override > 0:
+                width_final = width_override
+                height_final = int((aspect_height_ratio * width) / aspect_width_ratio)
             else:
-                height = base_size
-                width = int((aspect_width_ratio * height) / aspect_height_ratio)
+                base_size = 1024
+                if aspect_width_ratio <= aspect_height_ratio:
+                    width_final = base_size
+                    height_final = int((aspect_height_ratio * width) / aspect_width_ratio)
+                else:
+                    height_final = base_size
+                    width_final = int((aspect_width_ratio * height) / aspect_height_ratio)
+        else:
+            width_final = int(width)
+            height_final = int(height)
     
-        latent = torch.zeros([batch_size, 4, height // 8, width // 8], device=self.device)
+        latent = torch.zeros([batch_size, 4, height_final // 8, width_final // 8], device=self.device)
         return ({"samples":latent}, )
 
 
