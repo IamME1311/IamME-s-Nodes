c12e93fab4cc489b024a57cb9f11e569c2921d6e
IamME1311 <sarthak.agrawal1311@gmail.com>
2025-01-08 19:04:05 +0530

    added fine tuned LLama vision node

---
+++ py/llm_vision.py
@@ -0,0 +1,54 @@
+import requests
+import os
+
+import folder_paths
+from .utils import *
+
+
+
+class llm_vision:
+    def __init__(self):
+        self.input_dir = folder_paths.get_input_directory()
+
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))] 
+        return {
+            "required" : {
+                "image" : (sorted(files), {"image_upload":True}),
+                "seed" : ("INT", {"forceInput":True}),
+                "model_type" : (["llama", "gemini"], {"default":"gemini"}),
+                "prompt" : ("STRING", {"defualt":"", "multiline":True})
+            }
+        }
+    
+    CATEGORY = PACK_NAME
+    FUNCTION = "execute"
+    RETURN_TYPES = ("STRING","STRING",)
+    RETURN_NAMES = ("response", "debug",)
+
+    def execute(self, seed, model_type, image, prompt):
+        debug  = image # this image is saved to input folder
+        image_path = os.path.join(self.input_dir, image)
+        response = requests.post(f"http://192.168.0.83:8000/model/{model_type}/generate", 
+                         headers={"accept":"application/json"},
+                         files=[("uploaded_images", (image, open(image_path, "rb"), "image/png"))],
+                         params={"prompt": prompt}
+                         ).json()
+
+
+        return (response["message"], debug,)
+    
+
+
+NODE_CLASS_MAPPINGS = {"LLM Vision":llm_vision}
+NODE_DISPLAY_NAME_MAPPINGS = {"LLM Vision" : PACK_NAME + " LLM Vision"}
+
+#reference request code
+# response = requests.post("http://192.168.0.83:8000/model/gemini/generate", 
+#                          headers={"accept":"application/json"},
+#                          files=[("uploaded_images", ("flower.png", open("./flower.png", "rb"), "image/png"))],
+#                          params={"prompt":"describe the image"}
+#                          ).json()
+# response
