b6932e9a98e8798f50176338c24ef68bb67115e7
IamME1311 <sarthak.agrawal1311@gmail.com>
2025-06-06 15:52:17 +0530

    added patch_type node

---
+++ py/PatchType.py
@@ -0,0 +1,26 @@
+from .utils import PACK_NAME
+
+class Patch_Type:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required" : {
+                "patch_type": (["3:4","1:1", "9:16"], {
+                        "default": "3:4",
+                    }),
+            }
+        }
+    
+    CATEGORY = PACK_NAME
+    FUNCTION = "execute"
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("patch_type",)
+
+    def execute(self, patch_type:str):
+
+        return (patch_type,)
+    
+
+
+NODE_CLASS_MAPPINGS = {"patch_type" : Patch_Type}
+NODE_DISPLAY_NAME_MAPPINGS = {"patch_type" : PACK_NAME + " patch_type"}
